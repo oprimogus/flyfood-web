4b10eaf4a6c8585637d9c8bb4d21092351f6d4c1
oprimogus <gustavo.jesus@proton.me>
2025-03-12 23:20:12 -0300

    WIP: undo daisyUI config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -120,62 +120,62 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography"), daisyui, require("tailwindcss-animate")],
-  daisyui: {
-    themes: [
-      {
-        firstTheme: {
-          // Cores principais
-          primary: colorPalette.primary,
-          "primary-content": colorPalette.primaryContent,
-          "primary-focus": colorPalette.primaryFocus,
+  plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
+  // daisyui: {
+  //   themes: [
+  //     {
+  //       firstTheme: {
+  //         // Cores principais
+  //         primary: colorPalette.primary,
+  //         "primary-content": colorPalette.primaryContent,
+  //         "primary-focus": colorPalette.primaryFocus,
           
-          secondary: colorPalette.secondary,
-          "secondary-content": colorPalette.secondaryContent,
-          "secondary-focus": colorPalette.secondaryFocus,
+  //         secondary: colorPalette.secondary,
+  //         "secondary-content": colorPalette.secondaryContent,
+  //         "secondary-focus": colorPalette.secondaryFocus,
           
-          accent: colorPalette.accent,
-          "accent-content": colorPalette.accentContent,
-          "accent-focus": colorPalette.accentFocus,
+  //         accent: colorPalette.accent,
+  //         "accent-content": colorPalette.accentContent,
+  //         "accent-focus": colorPalette.accentFocus,
           
-          // Cores neutras
-          neutral: colorPalette.neutral,
-          "neutral-content": colorPalette.neutralContent,
-          "neutral-focus": colorPalette.neutralFocus,
+  //         // Cores neutras
+  //         neutral: colorPalette.neutral,
+  //         "neutral-content": colorPalette.neutralContent,
+  //         "neutral-focus": colorPalette.neutralFocus,
           
-          // Cores de background
-          "base-100": colorPalette.base100,
-          "base-200": colorPalette.base200,
-          "base-300": colorPalette.base300,
-          "base-content": colorPalette.baseContent,
+  //         // Cores de background
+  //         "base-100": colorPalette.base100,
+  //         "base-200": colorPalette.base200,
+  //         "base-300": colorPalette.base300,
+  //         "base-content": colorPalette.baseContent,
           
-          // Estados
-          success: colorPalette.success,
-          "success-content": colorPalette.successContent,
-          warning: colorPalette.warning,
-          "warning-content": colorPalette.warningContent,
-          error: colorPalette.error,
-          "error-content": colorPalette.errorContent,
-          info: colorPalette.info,
-          "info-content": colorPalette.infoContent,
+  //         // Estados
+  //         success: colorPalette.success,
+  //         "success-content": colorPalette.successContent,
+  //         warning: colorPalette.warning,
+  //         "warning-content": colorPalette.warningContent,
+  //         error: colorPalette.error,
+  //         "error-content": colorPalette.errorContent,
+  //         info: colorPalette.info,
+  //         "info-content": colorPalette.infoContent,
           
-          // Interface
-          bordered: colorPalette.border,
-          "bordered-hover": colorPalette.borderHover,
-          ring: colorPalette.ring,
-          disabled: colorPalette.disabled,
-          placeholder: colorPalette.placeholder,
-        },
-      },
-    ],
-    darkTheme: "dark",
-    base: true,
-    styled: true,
-    utils: true,
-    prefix: "",
-    logs: true,
-    themeRoot: ":root",
-  },
+  //         // Interface
+  //         bordered: colorPalette.border,
+  //         "bordered-hover": colorPalette.borderHover,
+  //         ring: colorPalette.ring,
+  //         disabled: colorPalette.disabled,
+  //         placeholder: colorPalette.placeholder,
+  //       },
+  //     },
+  //   ],
+  //   darkTheme: "dark",
+  //   base: true,
+  //   styled: true,
+  //   utils: true,
+  //   prefix: "",
+  //   logs: true,
+  //   themeRoot: ":root",
+  // },
 } satisfies Config
 
 export default config
